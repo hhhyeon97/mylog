b00d2a77b902330e943e5bb53284e07f07d074fb
minsol <kth083030@naver.com>
2024-10-20 11:12:52 +0900

    블로그 상세 페이지 뷰 구현

--- src/main/java/com/site/mylog/controller/BlogViewController.java
+++ src/main/java/com/site/mylog/controller/BlogViewController.java
@@ -1,11 +1,14 @@
 package com.site.mylog.controller;
 
+import com.site.mylog.domain.Article;
 import com.site.mylog.dto.ArticleListViewResponse;
+import com.site.mylog.dto.ArticleViewResponse;
 import com.site.mylog.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -31,6 +34,13 @@ public class BlogViewController {
         model.addAttribute("articles", articles); // 블로그 글 리스트 저장
 
         return "articleList"; // articleList.html 뷰 조회
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model){
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+        return "article";
     }
 
 }
--- src/main/java/com/site/mylog/domain/Article.java
+++ src/main/java/com/site/mylog/domain/Article.java
@@ -5,7 +5,13 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class) // 생성 및 수정 시간 자동 업데이트
 @Entity // 엔티티로 지정
 @NoArgsConstructor(access = AccessLevel.PROTECTED) // 파라미터 없는 생성자를 protected로 제한
 @Getter
@@ -33,4 +39,11 @@ public class Article {
         this.content = content;
     }
 
+    @CreatedDate // 엔티티가 생성될 때 생성 시간 저장
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate // 엔티티가 수정될 때 수정 시간 저장
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
